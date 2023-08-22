42547e57ca9fccf1b0936b680753c6825cf313c6
datphan <datphan0110@gmail.com>
2023-08-22 11:08:35 +0700

    update config

---
+++ charts/iorad/.dockerfiles/autorun.sh
@@ -0,0 +1,19 @@
+docker_exec() {
+  if hash ct 2>/dev/null; then
+    "$@"
+  else
+    docker exec --interactive ct "$@"
+  fi
+}
+
+docker_exec kubectl apply -f charts/iorad/.dockerfiles/pvc.yaml
+
+readonly CLUSTER_NAME=${1:-'chart-testing'}
+
+echo "docker pull gcr.io/teracy-iorad/iorad/iorad/app:develop"
+docker image pull gcr.io/teracy-iorad/iorad/iorad/app:develop
+
+docker tag gcr.io/teracy-iorad/iorad/iorad/app:develop iorad/app:develop
+
+echo "kind load docker-image gcr.io/teracy-iorad/iorad/iorad/app:develop --name $CLUSTER_NAME"
+kind load docker-image iorad/app:develop --name $CLUSTER_NAME
